69ac277f7cae83801fd7bad593ad4bb78d21b05a
ethernetexplorer <123896389+Paralaxial@users.noreply.github.com>
2023-01-31 19:11:32 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <p className="text-center p-6 text-6xl">Hello, World!</p>
+  )
+}
+
+export default Home
