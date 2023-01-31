951aee8be8d282fc16f53023c37a5af03f57d291
Paralaxial <123896389+Paralaxial@users.noreply.github.com>
2023-01-31 22:15:55 +0000

    e

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <p className="text-center p-6 text-6xl">Hello, World!</p>
+    <>
+      <p className="text-center p-6 font-medium text-4xl">Hello!</p>
+    </>
   )
 }
 
